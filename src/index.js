9ec87b0832cae21cfe5e3d0bca3fb7437bfa8e0c
sara toftegaard <satoftegaard@gmail.com>
2016-12-02 09:45:24 -0500

    "Hello, App App!"

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './index.html'
+import './styles/screen.sass'
+
+const main = () => {
+  document.querySelector('h1').textContent += '?'
+}
+
+document.addEventListener('DOMContentLoaded', main)
+
+if (module.hot) {
+  module.hot.dispose(() => window.location.reload())
+  module.hot.accept(err => console.error(err))
+}
